bab909cc746b39a1bc30eb5bcbc39e4e5ac807d7
andriykutsenko-student <syshmaks@gmail.com>
2026-05-25 00:49:43 +0300

    pr10: sock merchant - count matching sock pairs

--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,30 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
+use std::collections::HashMap;
+
+pub fn sock_merchant(ar: &[i32]) -> i32 {
+    let mut counts: HashMap<i32, i32> = HashMap::new();
+    for &s in ar {
+        *counts.entry(s).or_insert(0) += 1;
+    }
+    counts.values().map(|&c| c / 2).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sample() {
+        assert_eq!(sock_merchant(&[10, 20, 20, 10, 10, 30, 50, 10, 20]), 3);
+    }
+
+    #[test]
+    fn test_all_pairs() {
+        assert_eq!(sock_merchant(&[1, 1, 2, 2]), 2);
+    }
+
+    #[test]
+    fn test_no_pairs() {
+        assert_eq!(sock_merchant(&[1, 2, 3]), 0);
+    }
+}
