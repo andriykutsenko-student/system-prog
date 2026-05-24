f498e5d0d0b921ca59a0ad6e9fb6703308b595c0
andriykutsenko-student <syshmaks@gmail.com>
2026-05-25 00:39:30 +0300

    pr6: kangaroo - number line jumps implementation

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,35 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> &'static str {
+    if v1 == v2 {
+        if x1 == x2 { "YES" } else { "NO" }
+    } else if (x2 - x1) % (v1 - v2) == 0 && (x2 - x1) / (v1 - v2) > 0 {
+        "YES"
+    } else {
+        "NO"
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sample0() {
+        assert_eq!(kangaroo(0, 3, 4, 2), "YES");
+    }
+
+    #[test]
+    fn test_sample1() {
+        assert_eq!(kangaroo(0, 2, 5, 3), "NO");
+    }
+
+    #[test]
+    fn test_same_position() {
+        assert_eq!(kangaroo(5, 3, 5, 3), "YES");
+    }
+
+    #[test]
+    fn test_same_speed_different_pos() {
+        assert_eq!(kangaroo(0, 3, 5, 3), "NO");
+    }
+}
